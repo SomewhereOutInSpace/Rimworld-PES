9f79c35c3b5078b19a4e6173118495e142c1cf4b
James-DESKTOP <jameshaywood823@gmail.com>
2021-07-14 15:54:59 -0400

    Github push

---
+++ Source/PermadeathEnforcementSuite/PermadeathEnforcementSuite/LetterSaver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PermadeathEnforcementSuite
+{
+    [StaticConstructorOnStartup]
+    public static class LetterSaver
+    {
+
+        [HarmonyPatch(typeof(LetterStack), nameof(LetterStack.ReceiveLetter), new Type[] { typeof(Letter), typeof(string)})]
+        public static class Patch_LetterStack_ReceiveLetter {
+            public static void Postfix(Letter let, string debugInfo = null) {
+                if (Current.Game.Info.permadeathMode) {
+                    Log.Message("[PES] Autosaving...");
+                    Current.Game.autosaver.DoAutosave();
+                    Current.Game.autosaver.DoAutosave();
+                }
+            }
+        }
+
+        static LetterSaver() {
+            Log.Message("[PES] Okay, showtime!");          
+            Harmony har = new Harmony("PermadeathEnforcementSuite");
+            har.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ Source/PermadeathEnforcementSuite/PermadeathEnforcementSuite/SaveEncryptor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace PermadeathEnforcementSuite
+{
+    public static class SaveEncryptor
+    {
+        [HarmonyPatch(typeof(GameDataSaveLoader), nameof(GameDataSaveLoader.SaveGame), new Type[] { typeof(string)})]
+        public static class Patch_SaveGame
+        {
+            public static bool Prefix(ref string fileName)
+            {
+
+                try
+                {
+                    Traverse traverse = Traverse.CreateWithType("GameDataSaveLoader").Field("lastSaveTick");
+                    SafeSaver.Save(GenFilePaths.FilePathForSavedGame(fileName), "savegame", (Action)delegate
+                    {
+                        ScribeMetaHeaderUtility.WriteMetaHeader();
+                        Game target = Current.Game;
+                        Scribe_Deep.Look(ref target, "PESGame");
+                    });
+                    traverse.SetValue(Find.TickManager.TicksGame);
+                }
+
+                catch (Exception e)
+                {
+                    Log.Error("[PES] Exception in patched saving method!" + e);
+                }
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SavedGameLoaderNow), nameof(SavedGameLoaderNow.LoadGameFromSaveFileNow), new Type[] { typeof(string) })]
+        public static class Patch_LoadGame
+        {
+            public static bool Prefix(ref string fileName)
+            {
+                string text = LoadedModManager.RunningMods.Select((ModContentPack mod) => mod.PackageIdPlayerFacing).ToLineList("  - ");
+                Log.Message("Loading game from file " + fileName + " with mods:\n" + text);
+                DeepProfiler.Start("Loading game from file " + fileName);
+                Current.Game = new Game();
+                DeepProfiler.Start("InitLoading (read file)");
+                Scribe.loader.InitLoading(GenFilePaths.FilePathForSavedGame(fileName));
+                DeepProfiler.End();
+                try
+                {
+                    ScribeMetaHeaderUtility.LoadGameDataHeader(ScribeMetaHeaderUtility.ScribeHeaderMode.Map, logVersionConflictWarning: true);
+                    if (!Scribe.EnterNode("PESGame"))
+                    {
+                        Log.Error("[PES] Could not find PESGame XML node.");
+                        Scribe.ForceStop();
+                        GenScene.GoToMainMenu();
+                        Messages.Message("This save file is not PES-enabled. Please load a PES-enabled save file, or disable PES to access this save file.", MessageTypeDefOf.RejectInput);
+                        return false;
+                    }
+                    Current.Game = new Game();
+                    Current.Game.LoadGame();
+                }
+                catch (Exception)
+                {
+                    Scribe.ForceStop();
+                    throw;
+                }
+                PermadeathModeUtility.CheckUpdatePermadeathModeUniqueNameOnGameLoad(fileName);
+                DeepProfiler.End();
+                return false;
+            }
+        }
+    }
+}
